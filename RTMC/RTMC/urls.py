9ddefb950a1f93afea83bc15dead72e5d0713f60
FlyMen228 <flymen228@mail.ru>
2024-03-30 16:09:32 +0500

    favicon и футтер добавлены

--- RTMC/RTMC/urls.py
+++ RTMC/RTMC/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.views.defaults import page_not_found
 
 
 urlpatterns = [
@@ -7,3 +10,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# Для тестировки 404
+if settings.DEBUG:
+    
+    urlpatterns += [
+        path('test-404/', page_not_found, kwargs={'exception': Exception("Page not Found")}),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
